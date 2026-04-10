ffd5dc6377970ba738e4c16cd7d70220208dac6b
Junnister <157970058+Junnister@users.noreply.github.com>
2026-04-10 21:35:22 +0800

    Update firebase.js

--- frontend/lib/firebase.js
+++ frontend/lib/firebase.js
@@ -6,16 +6,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAY9sLrSS-N7Jw1Py8_VKU5EZt50qMEZqs",
-  authDomain: "eda-agent-backbone.firebaseapp.com",
-  projectId: "eda-agent-backbone",
-  storageBucket: "eda-agent-backbone.firebasestorage.app",
-  messagingSenderId: "622366923672",
-  appId: "1:622366923672:web:71f53204d88d6e1f2ae9ed",
-  measurementId: "G-3C3WFGWH6R"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
